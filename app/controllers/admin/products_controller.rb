b1204f831c86dd9b6ec5f0afd124e58b9ba782d8
edwardxywang@gmail.com <edwardxywang@gmail.com>
2017-02-19 01:15:01 +0000

    admin http authentication

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,8 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: ENV["WEB_USERNAME"], password: ENV["WEB_PASSWORD"]
+
+
   def index
     @products = Product.order(id: :desc).all
   end
