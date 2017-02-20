1a51aa7bf13dbe18c13fefd66d4e683d6137db10
edwardxywang@gmail.com <edwardxywang@gmail.com>
2017-02-20 22:56:52 +0000

    test the product model

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,26 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
+
+  before(:each) do
+    @cat = Category.new
+  end
+
   describe "validations" do
     it "should create a product" do
-      @cat = Category.new
       @product = @cat.products.new(name: "charger", price: 120, quantity: 100)
       expect(@product).to be_valid
     end
     it "misses name field, therefore should not create a product" do
-      @cat = Category.new
       @product = @cat.products.new(price: 120, quantity: 100)
       expect(@product).to_not be_valid
     end
     it "misses price field, therefore should not create a product" do
-      @cat = Category.new
       @product = @cat.products.new(name: "charger", quantity: 100)
       expect(@product).to_not be_valid
     end
     it "misses quantity field, therefore should not create a product" do
-      @cat = Category.new
       @product = @cat.products.new(name: "charger", price: 120)
       expect(@product).to_not be_valid
+    end
+    it "misses quantity field, therefore should not create a product" do
+      @product = @cat.products.new(name: "charger", price: 120)
+      @product.save
+      expect(@product.errors.messages[:quantity][0]).to eq("can't be blank")
     end
   end
 end
