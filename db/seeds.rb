54d4a27b354120cd896e32b31573466db0160c65
edwardxywang@gmail.com <edwardxywang@gmail.com>
2017-02-22 03:29:01 +0000

    addind seeds to prod (just for demo- bad idea)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,17 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 puts "Seeding Data ..."
-
+dsf
 # Helper functions
 def open_asset(file_name)
   File.open(Rails.root.join('db', 'seed_assets', file_name))
 end
 
 # Only run on development (local) instances not on production, etc.
-unless Rails.env.development?
-  puts "Development seeds only (for now)!"
-  exit 0
-end
+# unless Rails.env.development?
+#   puts "Development seeds only (for now)!"
+#   exit 0
+# end
 
 # Let's do this ...
 
